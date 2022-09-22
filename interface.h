bfefaea9e4a0e4747b658dc54822aef0f261f6ca
basava <basava@node-0.basava.uwmadison744-f21-pg0.wisc.cloudlab.us>
2022-09-22 14:51:38 -0500

    comm code for pipe,sm, and socket

--- interface.h
+++ interface.h
@@ -16,9 +16,33 @@
 #define S_512KB 1024*512
 #define S_1MB   1024*1024
 
+#define BUFFER_SZ 1000000
 
 //4, 16, 64, 256, 1K, 4K, 16K, 64K, 256K, and 512K bytes.
 
+/* Headers for reporting rdtsc cycles
+ * Usage - 
+ * int tick_start = getticks();
+ * int tick_end = getticks();
+ * cycles taken: (tick_end - tick_start)/2.0 [accounting for RTT]
+ * time taken ~ (tick_end - tick_start)/(2.0*cpu_freq_hz)
+ */
+
+typedef unsigned long long ticks;
+// change this variable based on the evaluation system, cat /proc/cpuinfo | grep 'cpu MHz'
+// put the value in Hz (grep value * 1000) 
+int cpu_freq_hz = 1200887;
+
+/* ticks code taken from https://www.unix.com/programming/81639-rdtsc-use-c.html
+*/
+static __inline__ ticks getticks(void)
+{
+     unsigned a, d;
+     asm("cpuid");
+     asm volatile("rdtsc" : "=a" (a), "=d" (d));
+
+     return (((ticks)a) | (((ticks)d) << 32));
+}
 
 /**
  * @return 1 for success, 0 o/w
