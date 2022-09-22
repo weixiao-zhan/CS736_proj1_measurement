6ab3cc6a3b38b268331970f9b6b7b977c4c3e9b8
Weixiao Zhan <weixiao.zhan00@gmail.com>
2022-09-22 09:31:46 -0500

    pipe imp

--- implement_weixiao/run.c
+++ implement_weixiao/run.c
@@ -1,7 +1,62 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
 #include "../interface.h"
 
+int main(int argc, char** argv) {
+    int message_size=1024*8; // this implementation is limited to 8kB
+    int buffer_size = 64;
+    if (argc >= 2){
+        message_size = atoi(argv[1]);
+    }
+    if (argc >= 3){
+        buffer_size = atoi(argv[2]);
+    }
+    run_pipe(message_size, buffer_size);
+}
 
-int run_pipe(size_t message_size, size_t package_size);
+int write_helper(int fd, size_t message_size, size_t buffer_size){
+    char buf[buffer_size];
+    memset(buf, 100, buffer_size);
+    size_t writen = 0;
+    while(writen < message_size){
+        writen += write(fd, buf, buffer_size);
+    }
+    return writen;
+}
+
+int read_helper(int fd, size_t message_size, size_t buffer_size) {
+    char buf[buffer_size];
+    size_t readed = 0;
+    while (readed < message_size){
+        readed += read(fd, buf, buffer_size);
+    }
+    return readed;
+}
+
+int run_pipe(size_t message_size, size_t package_size){
+    int fd[2];
+    pipe(fd);
+
+    pid_t childpid;
+    if ((childpid = fork()) == -1) {
+        perror("fork");
+        return(-1);
+    }
+
+    if (childpid == 0) { /* Child process */
+        read_helper(fd[0], message_size, package_size);
+        write_helper(fd[1], message_size, package_size);
+        exit(0);
+    } else { /* Parent process */
+        write_helper(fd[1], message_size, package_size);
+        int n = read_helper(fd[0], message_size, package_size);
+        printf("Received: %d", n);
+    }
+    return(0);
+}
+
 int run_socket(size_t message_size, size_t package_size);
 int run_shared_mem(size_t message_size, size_t package_size);
--- interface.h
+++ interface.h
@@ -2,6 +2,24 @@
 
 #ifndef INTERFACE_H
 #define INTERFACE_H
+
+
+#define S_4B    4
+#define S_16B   16
+#define S_64B   64
+#define S_256B  256
+#define S_1KB   1024
+#define S_4KB   1024*4
+#define S_16KB  1024*16
+#define S_64KB  1024*64
+#define S_256KB 1024*256
+#define S_512KB 1024*512
+#define S_1MB   1024*1024
+
+
+//4, 16, 64, 256, 1K, 4K, 16K, 64K, 256K, and 512K bytes.
+
+
 /**
  * @return 1 for success, 0 o/w
  * @param message_size in bytes
